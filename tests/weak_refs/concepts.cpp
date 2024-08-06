67f1e604fe24d1e95bd565010832c1c84e87fd44
Fred Emmott <git@fredemmott.com>
2024-08-06 14:04:58 -0500

    ✅ Add stub of weak_refs::concepts test

---
+++ tests/weak_refs/concepts.cpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2024 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include <FredEmmott/weak_refs.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace FredEmmott::weak_refs;
+
+TEST_CASE("raw pointers") {
+  STATIC_CHECK(!strong_ref<int*>);
+  STATIC_CHECK(!weak_ref<int*>);
+}
