898d1437351ea366ec80586567ce756563093567
Fred Emmott <git@fredemmott.com>
2024-08-07 14:11:14 -0500

    🚚 split out common code to detail namespace and file

--- include/FredEmmott/weak_refs/bind_refs_front.hpp
+++ include/FredEmmott/weak_refs/bind_refs_front.hpp
@@ -2,17 +2,17 @@
 // SPDX-License-Identifier: MIT
 #pragma once
 
-#include "bind_maybe_refs_front.hpp"
-
 #include <tuple>
 #include <utility>
+
+#include "detail/front_binder.hpp"
 
 namespace FredEmmott::weak_refs {
 
 template <class F, convertible_to_weak_ref... Binds>
 auto bind_refs_front(F&& f, Binds&&... binds) {
-  const auto ret = bind_maybe_refs_detail::front_binder<
-    bind_maybe_refs_detail::NotARefBehavior::Error,
+  const auto ret = weak_refs_detail::front_binder<
+    weak_refs_detail::NotARefBehavior::Error,
     std::decay_t<F>,
     std::decay_t<Binds>...>(std::forward<F>(f), std::forward<Binds>(binds)...);
   static_assert(decltype(ret)::refs_required_v);
