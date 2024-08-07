79f7260f350fc8d30c34fb9b6ccd9fba115a17da
Fred Emmott <git@fredemmott.com>
2024-08-07 14:21:08 -0500

    ✅ also test common behavior of maybe_weak_refs

--- tests/weak_refs/bind_maybe_refs_front.cpp
+++ tests/weak_refs/bind_maybe_refs_front.cpp
@@ -8,7 +8,7 @@ using namespace FredEmmott::weak_refs;
 
 template <class... Args>
 auto bind_function_under_test(Args&&... args) {
-  return bind_refs_front(std::forward<Args>(args)...);
+  return bind_maybe_refs_front(std::forward<Args>(args)...);
 }
 
 #include "common/bind_refs.hpp"
---
+++ tests/weak_refs/common/bind_refs.hpp
@@ -0,0 +1,109 @@
+// Copyright (C) 2024 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: MIT
+#include <FredEmmott/weak_refs.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace FredEmmott::weak_refs;
+
+TEST_CASE("with a single shared_ptr") {
+  auto a = std::make_shared<int>(123);
+
+  bool invoked = false;
+  auto f = bind_function_under_test(
+    [&invoked](auto a) {
+      CHECK(a);
+      CHECK(*a == 123);
+      invoked = true;
+    },
+    a);
+  CHECK(!invoked);
+  f();
+  CHECK(invoked);
+}
+
+TEST_CASE("with two shared_ptrs") {
+  auto a = std::make_shared<int>(123);
+  auto b = std::make_shared<int>(456);
+
+  bool invoked = false;
+  auto f = bind_function_under_test(
+    [&invoked](auto a, auto b) {
+      CHECK(a);
+      CHECK(*a == 123);
+      CHECK(b);
+      CHECK(*b == 456);
+      invoked = true;
+    },
+    a,
+    b);
+  CHECK(!invoked);
+  f();
+  CHECK(invoked);
+}
+
+TEST_CASE("unseating the first shared_ptr") {
+  auto a = std::make_shared<int>(123);
+  auto b = std::make_shared<int>(456);
+
+  bool invoked = false;
+  auto f = bind_function_under_test(
+    [&invoked](auto a, auto b) {
+      CHECK(false);
+      invoked = true;
+    },
+    a,
+    b);
+  CHECK(!invoked);
+  a = nullptr;
+  f();
+  CHECK(!invoked);
+}
+
+TEST_CASE("unseating the second shared_ptr") {
+  auto a = std::make_shared<int>(123);
+  auto b = std::make_shared<int>(456);
+  a = std::make_shared<int>(123);
+  bool invoked = false;
+  auto f = bind_function_under_test(
+    [&invoked](auto a, auto b) {
+      CHECK(false);
+      invoked = true;
+    },
+    a,
+    b);
+  CHECK(!invoked);
+  b = nullptr;
+  f();
+  CHECK(!invoked);
+}
+
+TEST_CASE("promoting weak_ptrs") {
+  auto strong = std::make_shared<int>(123);
+
+  bool invoked = false;
+  bind_function_under_test(
+    [&invoked](auto it) {
+      STATIC_CHECK(std::same_as<decltype(it), std::shared_ptr<int>>);
+      CHECK(*it == 123);
+      invoked = true;
+    },
+    std::weak_ptr {strong})();
+  CHECK(invoked);
+}
+
+TEST_CASE("stale weak_ptrs") {
+  auto strong = std::make_shared<int>(123);
+
+  bool invoked = false;
+  auto f = bind_function_under_test(
+    [&invoked](auto it) {
+      CHECK(false);
+      invoked = true;
+    },
+    std::weak_ptr {strong});
+  CHECK(!invoked);
+  strong = nullptr;
+  f();
+  CHECK(!invoked);
+}
