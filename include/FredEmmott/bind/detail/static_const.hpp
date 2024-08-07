716bd97b65f0f46b8438d425dbddd71ab0021a34
Fred Emmott <git@fredemmott.com>
2024-08-07 14:46:06 -0500

    🔥remove `bind` component
    Let's do composition differently

--- include/FredEmmott/bind/detail/static_const.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2024 Fred Emmott <fred@fredemmott.com>
-// SPDX-License-Identifier: MIT
-#pragma once
-
-namespace FredEmmott::bind_detail {
-template <class T>
-constexpr T static_const {};
-}
