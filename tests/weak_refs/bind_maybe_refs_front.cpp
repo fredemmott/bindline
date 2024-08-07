733261ddad97d3eb6c0830f918d31c9b8886b917
Fred Emmott <git@fredemmott.com>
2024-08-07 14:41:12 -0500

    Test maybe_refs behavior

--- tests/weak_refs/bind_maybe_refs_front.cpp
+++ tests/weak_refs/bind_maybe_refs_front.cpp
@@ -12,3 +12,22 @@ auto bind_function_under_test(Args&&... args) {
 }
 
 #include "common/bind_refs.hpp"
+
+TEST_CASE("with a mix of refs and not-refs") {
+  auto ref = std::make_shared<int>(123);
+  auto not_ref = std::make_unique<int>(456);
+
+  bool invoked = false;
+  bind_maybe_refs_front(
+    [&invoked](auto ref, auto not_ref) {
+      STATIC_CHECK(std::same_as<decltype(ref), std::shared_ptr<int>>);
+      STATIC_CHECK(std::same_as<decltype(not_ref), int*>);
+      CHECK(ref);
+      CHECK(*ref == 123);
+      CHECK(*not_ref == 456);
+      invoked = true;
+    },
+    ref,
+    not_ref.get())();
+  CHECK(invoked);
+}
