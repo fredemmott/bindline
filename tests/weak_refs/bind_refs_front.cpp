047ef189a26da7a1b249cbb6d785a4808d46fe62
Fred Emmott <git@fredemmott.com>
2024-08-07 13:42:35 -0500

    🚚 Split to multiple test cases

--- tests/weak_refs/bind_refs_front.cpp
+++ tests/weak_refs/bind_refs_front.cpp
@@ -6,71 +6,74 @@
 
 using namespace FredEmmott::weak_refs;
 
-TEST_CASE("with std::shared_ptr") {
+TEST_CASE("with a single shared_ptr") {
   auto a = std::make_shared<int>(123);
 
-  {
-    bool invoked = false;
-    auto f = bind_refs_front(
-      [&invoked](auto a) {
-        CHECK(a);
-        CHECK(*a == 123);
-        invoked = true;
-      },
-      a);
-    CHECK(!invoked);
-    f();
-    CHECK(invoked);
-  }
+  bool invoked = false;
+  auto f = bind_refs_front(
+    [&invoked](auto a) {
+      CHECK(a);
+      CHECK(*a == 123);
+      invoked = true;
+    },
+    a);
+  CHECK(!invoked);
+  f();
+  CHECK(invoked);
+}
 
+TEST_CASE("with two shared_ptrs") {
+  auto a = std::make_shared<int>(123);
   auto b = std::make_shared<int>(456);
-  {
-    bool invoked = false;
-    auto f = bind_refs_front(
-      [&invoked](auto a, auto b) {
-        CHECK(a);
-        CHECK(*a == 123);
-        CHECK(b);
-        CHECK(*b == 456);
-        invoked = true;
-      },
-      a,
-      b);
-    CHECK(!invoked);
-    f();
-    CHECK(invoked);
-  }
 
-  // Unseat first
-  {
-    bool invoked = false;
-    auto f = bind_refs_front(
-      [&invoked](auto a, auto b) {
-        CHECK(false);
-        invoked = true;
-      },
-      a,
-      b);
-    CHECK(!invoked);
-    a = nullptr;
-    f();
-    CHECK(!invoked);
-  }
+  bool invoked = false;
+  auto f = bind_refs_front(
+    [&invoked](auto a, auto b) {
+      CHECK(a);
+      CHECK(*a == 123);
+      CHECK(b);
+      CHECK(*b == 456);
+      invoked = true;
+    },
+    a,
+    b);
+  CHECK(!invoked);
+  f();
+  CHECK(invoked);
+}
 
-  // Unseat second
-  {
-    a = std::make_shared<int>(123);
-    bool invoked = false;
-    auto f = bind_refs_front(
-      [&invoked](auto a, auto b) {
-        CHECK(false);
-        invoked = true;
-      },
-      a,
-      b);
-    CHECK(!invoked);
-    b = nullptr;
-    f();
-    CHECK(!invoked);
-  }
+TEST_CASE("unseating the first shared_ptr") {
+  auto a = std::make_shared<int>(123);
+  auto b = std::make_shared<int>(456);
+
+  bool invoked = false;
+  auto f = bind_refs_front(
+    [&invoked](auto a, auto b) {
+      CHECK(false);
+      invoked = true;
+    },
+    a,
+    b);
+  CHECK(!invoked);
+  a = nullptr;
+  f();
+  CHECK(!invoked);
+}
+
+TEST_CASE("unseating the second shared_ptr") {
+  auto a = std::make_shared<int>(123);
+  auto b = std::make_shared<int>(456);
+  a = std::make_shared<int>(123);
+  bool invoked = false;
+  auto f = bind_refs_front(
+    [&invoked](auto a, auto b) {
+      CHECK(false);
+      invoked = true;
+    },
+    a,
+    b);
+  CHECK(!invoked);
+  b = nullptr;
+  f();
+  CHECK(!invoked);
 }
