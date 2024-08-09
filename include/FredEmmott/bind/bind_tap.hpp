ff8a3bd811a776c09d02b273a9759b2efcd80199
Fred Emmott <git@fredemmott.com>
2024-08-08 21:23:55 -0500

    Make bind_tap directly invocable if first arg is a callable

--- include/FredEmmott/bind/bind_tap.hpp
+++ include/FredEmmott/bind/bind_tap.hpp
@@ -4,12 +4,12 @@
 
 #include "bindable_t.hpp"
 
-namespace FredEmmott::bind {
+namespace FredEmmott::bind_detail {
 
 template <class TTap>
-struct bind_tap : public bindable_t {
-  bind_tap() = delete;
-  bind_tap(TTap tap) : mTap(std::forward<TTap>(tap)) {
+struct bind_tap_t : public ::FredEmmott::bind::bindable_t {
+  bind_tap_t() = delete;
+  bind_tap_t(TTap tap) : mTap(std::forward<TTap>(tap)) {
   }
 
   auto bind_to(auto&& f) const {
@@ -23,5 +23,15 @@ struct bind_tap : public bindable_t {
  private:
   std::decay_t<TTap> mTap;
 };
+
+}// namespace FredEmmott::bind_detail
+
+namespace FredEmmott::bind {
+template <class... Args>
+[[nodiscard]]
+auto bind_tap(Args&&... args) {
+  using namespace bind_detail;
+  return make_bindable<bind_tap_t>(std::forward<Args>(args)...);
+}
 
 }// namespace FredEmmott::bind
