4d8d39a4c28080d3e7d8fc4637ebd7c388315ba2
Fred Emmott <git@fredemmott.com>
2024-08-10 15:08:07 -0500

    ✅ Fix tests for std::invocable

--- include/FredEmmott/weak_refs/detail/binder_t.hpp
+++ include/FredEmmott/weak_refs/detail/binder_t.hpp
@@ -17,42 +17,63 @@ using namespace FredEmmott::weak_refs;
 template <class TTraits, class TFn, weak_ref... TBinds>
   requires(sizeof...(TBinds) >= 1)
 struct binder_t {
-  using function_t = TFn;
+  static_assert(std::same_as<TFn, std::decay_t<TFn>>);
+  static_assert((std::same_as<TBinds, std::decay_t<TBinds>> && ...));
 
   binder_t() = delete;
   template <class TInitFn, class... TInitBinds>
   constexpr binder_t(TInitFn&& fn, TInitBinds... binds)
     : mFn(std::forward<TInitFn>(fn)),
-      mBinds(
-        std::make_tuple(make_weak_ref(std::forward<TInitBinds>(binds))...)) {
+      mBinds(make_weak_ref(std::forward<TInitBinds>(binds))...) {
   }
 
+  using strong_binds_t
+    = std::tuple<decltype(lock_weak_ref(std::declval<TBinds>()))...>;
+
+  template <class... Unbound>
+  using final_args_t = decltype(TTraits::make_args_tuple(
+    std::declval<strong_binds_t>(),
+    std::declval<Unbound>()...));
+
+  template <class... TArgs>
+    requires std::invocable<TFn, TArgs...>
+  static std::true_type can_apply_t(std::tuple<TArgs...>) {
+    return {};
+  };
+  static std::false_type can_apply_t(...) {
+    return {};
+  };
+
+  template <class TArgs>
+  static constexpr bool can_apply_v
+    = decltype(can_apply_t(std::declval<TArgs>()))::value;
+
   template <class... UnboundArgs>
+    requires(can_apply_v<final_args_t<UnboundArgs...>>)
   constexpr void operator()(UnboundArgs&&... unboundArgs) const {
-    auto strong_binds = std::apply(
-      [](const auto&... binds) {
-        return std::make_tuple(lock_weak_ref(binds)...);
+    strong_binds_t strong_binds = std::apply(
+      []<class... CallArgs>(CallArgs&&... binds) -> decltype(auto) {
+        return std::make_tuple(lock_weak_ref(std::forward<CallArgs>(binds))...);
       },
       mBinds);
     const auto all_live = std::apply(
-      [](const auto&... binds) {
-        static_assert((strong_ref<decltype(binds)> && ...));
-        return (static_cast<bool>(binds) && ...);
+      []<strong_ref... CallArgs>(CallArgs&&... binds) {
+        return (static_cast<bool>(std::forward<CallArgs>(binds)) && ...);
       },
       strong_binds);
     if (!all_live) {
       return;
     }
 
-    std::apply(
-      mFn,
-      TTraits::make_args_tuple(
-        strong_binds, std::forward<UnboundArgs>(unboundArgs)...));
+    final_args_t<UnboundArgs...> final_args = TTraits::make_args_tuple(
+      std::move(strong_binds), std::forward<UnboundArgs>(unboundArgs)...);
+
+    std::apply(mFn, std::move(final_args));
   }
 
  private:
-  const TFn mFn;
-  const std::tuple<TBinds...> mBinds;
+  TFn mFn;
+  std::tuple<TBinds...> mBinds;
 };
 
 }// namespace FredEmmott::weak_refs_detail
