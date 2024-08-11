afd419b8624ea5cc74ab81cbf745e49ac5a57a5d
Fred Emmott <git@fredemmott.com>
2024-08-11 11:47:30 -0500

    🚚 inline short header file only used in one place

--- include/FredEmmott/composable_bind/drop_extra_back.hpp
+++ include/FredEmmott/composable_bind/drop_extra_back.hpp
@@ -2,8 +2,18 @@
 // SPDX-License-Identifier: MIT
 #pragma once
 
-#include "detail/drop_any_arg_traits.hpp"
 #include "extension_api/drop_back_bindable_t.hpp"
+
+namespace FredEmmott::composable_bind_detail {
+
+struct drop_any_arg_traits {
+  template <size_t DropCount, class T>
+  static constexpr bool can_drop_v = true;
+
+  static constexpr size_t minimum_dropped_v = 0;
+};
+
+}// namespace FredEmmott::composable_bind_detail
 
 namespace FredEmmott::composable_bind {
 
